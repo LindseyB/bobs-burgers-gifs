3e735fd79c41160804207960777c789bed21dce9
Lindsey B <hazardous.narayan@gmail.com>
2021-05-22 13:43:38 -0400

    Update to extract the subtitles

--- scripts/extract_subtitles.rb
+++ scripts/extract_subtitles.rb
@@ -1,8 +1,11 @@
+ #!/usr/bin/env ruby
 # Assume the first argument is the directory we want to traverse
 DIRECTORY = ARGV[0]
 
+puts "Directory is #{DIRECTORY}"
+Dir.chdir(DIRECTORY)
 Dir.glob("**/*.mkv") do |file|
-  puts file.expand_path
-  puts file.basename
-  #system("ffmpeg", "-i", file.expand_path, "-o", "#{file.basename}.srt")
+  path = File.absolute_path(file)
+  basename = File.basename(file, ".*") 
+  system("ffmpeg", "-i", path, "#{basename}.srt")
 end
